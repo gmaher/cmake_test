ac3311579cbe955ec847421f23a61dd024ed1ab9
gabriel <gmaher2@hotmail.com>
2017-08-23 19:17:05 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "mymath.h"
+int main(){
+  std::cout << "This is the main file\n";
+  std::cout << "1.0 + 2.0 = " << add(1.0,2.0);
+  return 0;
+}
---
+++ submodule_1/mymath.cpp
@@ -0,0 +1,5 @@
+#include "math.h"
+
+double add(double x, double y){
+  return x+y;
+}
